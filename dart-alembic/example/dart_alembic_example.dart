294d26de1e0d56b3d4011882b33bb366abb160e1
Antony Sequeira <asequeira.open@gmail.com>
2021-06-02 19:35:27 -0700

    created by dart
    dart create --template=package-simple  dart-alembic

---
+++ dart-alembic/example/dart_alembic_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_alembic/dart_alembic.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ dart-alembic/lib/dart_alembic.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_alembic;
+
+export 'src/dart_alembic_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
